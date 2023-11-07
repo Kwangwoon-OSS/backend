fd429328be54fbdc460a9e5c6aecab963586e503
kwongio <rldh11111@naver.com>
2023-11-07 19:49:38 +0900

    [Feat] 프로필 조회 기능 추가

--- src/main/java/com/example/be_kwangwoon/domain/user/controller/response/UserProfileResponse.java
+++ src/main/java/com/example/be_kwangwoon/domain/user/controller/response/UserProfileResponse.java
@@ -1,12 +1,27 @@
 package com.example.be_kwangwoon.domain.user.controller.response;
 
 import com.example.be_kwangwoon.domain.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class UserProfileResponse {
+    private final String nickname;
+    private final String email;
+    private final String departmentName;
 
     public static UserProfileResponse from(User user) {
-        return null;
+        return UserProfileResponse.builder()
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .departmentName(user.getDepartment().getName())
+                .build();
+    }
+
+    @Builder
+    public UserProfileResponse(String nickname, String email, String departmentName) {
+        this.nickname = nickname;
+        this.email = email;
+        this.departmentName = departmentName;
     }
 }
