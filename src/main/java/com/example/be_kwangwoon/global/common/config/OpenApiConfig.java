89f967a0e8396fd1251d04041f8821c359f44164
kwongio <rldh11111@naver.com>
2023-11-07 16:13:08 +0900

    [Release] 배포 테스트

---
+++ src/main/java/com/example/be_kwangwoon/global/common/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.example.be_kwangwoon.global.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://www.petmily.site", description = "production server url"),
+        @Server(url = "http://localhost:8080", description = "dev server url")
+})
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("투게더 API")
+                .version("v1.0.0")
+                .description("투게더 API");
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("Bearer 토큰", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer 토큰"))
+                .info(info);
+    }
+
+
+}
