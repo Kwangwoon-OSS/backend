407a0d43dc2fae5e396b8988308e179c794a2f34
kwongio <rldh11111@naver.com>
2023-11-14 20:57:43 +0900

    [refactor] 이메일 인증 권한 처리 삭제

--- src/main/java/com/example/be_kwangwoon/global/common/config/SecurityConfig.java
+++ src/main/java/com/example/be_kwangwoon/global/common/config/SecurityConfig.java
@@ -64,8 +64,6 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/posts/**/**", "DELETE")).authenticated());
         http.authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/posts/**/**", "POST")).authenticated());
         http.authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/posts/**/**", "PUT")).authenticated());
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/users/email/verify", "POST")).authenticated());
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/users/email/cert", "POST")).authenticated());
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         return http.build();
     }
