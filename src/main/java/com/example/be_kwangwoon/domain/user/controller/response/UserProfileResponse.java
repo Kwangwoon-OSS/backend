156cc766683427350aa111063cec666d6690b3ca
kwongio <rldh11111@naver.com>
2023-11-19 21:51:21 +0900

    [refactor] 프로필 조회 userId 필드 추가

--- src/main/java/com/example/be_kwangwoon/domain/user/controller/response/UserProfileResponse.java
+++ src/main/java/com/example/be_kwangwoon/domain/user/controller/response/UserProfileResponse.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 
 @Getter
 public class UserProfileResponse {
+    private final Long userId;
     private final String nickname;
     private final String departmentName;
     private final String introduction;
 
     public static UserProfileResponse from(User user) {
         return UserProfileResponse.builder()
+                .userId(user.getId())
                 .nickname(user.getNickname())
                 .departmentName(user.getDepartment().getName())
                 .introduction(user.getIntroduction())
@@ -20,7 +22,8 @@ public class UserProfileResponse {
     }
 
     @Builder
-    public UserProfileResponse(String nickname, String departmentName, String introduction) {
+    public UserProfileResponse(Long userId, String nickname, String departmentName, String introduction) {
+        this.userId = userId;
         this.nickname = nickname;
         this.departmentName = departmentName;
         this.introduction = introduction;
